4600c1f0b2363b526431df923d8ecb33f2175794
Uttam Kumar <uttammaurya377@gmail.com>
2025-04-23 01:30:51 +0530

    done

--- client/src/pages/students/MyEnrollments.jsx
+++ client/src/pages/students/MyEnrollments.jsx
@@ -1,48 +1,76 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../../context/AppContext'
-
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../../context/AppContext';
+import { useNavigate } from 'react-router-dom';
+import {Line} from 'rc-progress'
+import Footer from '../../components/students/Footer'
 const MyEnrollments = () => {
-  const {enrolledCourses,calculateCourseDuration}=useContext(AppContext)
+
+  const navigate=useNavigate()
+  const { enrolledCourses, calculateCourseDuration } = useContext(AppContext);
+  const [progressArray,setProgressArray]=useState([
+    {lectureCompleted:2 ,totalLectures:4},
+    {lectureCompleted:3 ,totalLectures:5},
+    {lectureCompleted:4 ,totalLectures:6},
+    {lectureCompleted:1 ,totalLectures:4},
+    {lectureCompleted:5 ,totalLectures:3},
+    {lectureCompleted:8 ,totalLectures:7},
+    {lectureCompleted:6 ,totalLectures:8},
+    {lectureCompleted:2 ,totalLectures:6},
+    {lectureCompleted:1 ,totalLectures:10},
+    {lectureCompleted:0 ,totalLectures:5},
+    {lectureCompleted:2 ,totalLectures:7},
+    {lectureCompleted:2 ,totalLectures:4},
+    {lectureCompleted:2 ,totalLectures:2},
+    {lectureCompleted:2 ,totalLectures:5},
+    // {lectureCompleted:2,totalLectures:4},
+
+  ])
+
+
+
   return (
     <>
       <div className='md:px-36 px-8 pt-10'>
-  <h1 className='text-2xl font-semibold'>My Enrollments</h1>
-  <table className='md:table-auto table-fixed w-full overflow-hidden border mt-10'>
-    <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left max-sm:hidden'>
-      <tr>
-        <th className='px-4 py-3 font-semibold truncate'>Course</th>
-        <th className='px-4 py-3 font-semibold truncate'>Duration</th>
-        <th className='px-4 py-3 font-semibold truncate'>Completed</th>
-        <th className='px-4 py-3 font-semibold truncate'>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      {enrolledCourses.map((course,index)=>{
-        <tr key={index}>
-          <td>
-            <img src={course.courseThubnail} alt="" className='w-14 sm:w-24 md:w-28' />
-            <div>
-              <p>{course.courseTitle}</p>
-            </div>
-          </td>
-          <td>
-          {calculateCourseDuration(course)}
-          </td>
-          <td>
-          4/10 <span>Lectures</span>
-          </td>
-          <td>
-            <button>On Goind</button>
-          </td>
-        </tr>
+        <h1 className='text-2xl font-semibold'>My Enrollments</h1>
+        <table className='md:table-auto table-fixed w-full overflow-hidden border mt-10'>
+          <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left max-sm:hidden'>
+            <tr>
+              <th className='px-4 py-3 font-semibold truncate'>Course</th>
+              <th className='px-4 py-3 font-semibold truncate'>Duration</th>
+              <th className='px-4 py-3 font-semibold truncate'>Completed</th>
+              <th className='px-4 py-3 font-semibold truncate'>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {enrolledCourses.map((course, index) => (
+              <tr className='border-b border-gray-500/20' key={index}>
+                <td className='flex items-center gap-4 p-2'>
+                  <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28' />
+                  <div className='flex-1'>
+                    <p className='mb-1 max-sm:text-sm'>{course.courseTitle}</p>
+                    <Line strokeWidth={2} percent={progressArray[index]?(progressArray[index].lectureCompleted*100)/progressArray[index].totalLectures:0} className='bg-gray-300 rounded-full' />
+                  </div>
+                </td>
+                <td className='px-4 py-3 max-sm:hidden'>
+                  {calculateCourseDuration(course)}
+                </td>
+                <td className='px-4 py-3 max-sm:hidden'>
+                  {progressArray[index]&& `${progressArray[index].lectureCompleted}/${progressArray[index].lectureCompleted}`}<span>Lectures</span>
+                </td>
+                <td className='px-4 py-3 max-sm:text-right'>
+                  <button onClick={()=>navigate('/player/+course._id')} className='bg-blue-600 text-white-700 cursor-pointer px-3 py-1 rounded'>
+                    {progressArray[index]&& progressArray[index].lectureCompleted/progressArray[index].totalLectures===1 ? 'Completed':'On Going'}
 
-      })}
-    </tbody>
-  </table>
-</div>
-
+                    </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default MyEnrollments
+export default MyEnrollments;
--- client/src/pages/students/Player.jsx
+++ client/src/pages/students/Player.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const Player = () => {
+  const [entrolledCourse,calculateChapterTime]=useContext(AppContext)
   return (
-    <div>
-      <h1>Player</h1>
+    <>
+  <div className='p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36'>
+    
+    <div className='text-gray-800'>
+      <h2 className='text-xl font-semibold'>Course Structure</h2>
+      <div className="pt-5">
+                        {courseData.courseContent.map((chapter,index)=>(
+                          <div
+                          className='border border-gray-300 bg-white mb-2 rouded'
+                          key={index}>
+                            <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'
+                            onClick={()=>toggleSection(index)}
+                            >
+                              <div className='flex items-center gap-2'>
+      
+                                <img
+                                className={`transform transition-transform ${openSection[index]? 'rotate-180':''}`}
+                                src={assets.down_arrow_icon} alt="arrow icon" />
+                                <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
+      
+                              </div>
+                              <p className='text-sm md:text-defauld'>{chapter.chapterContent.length}lectures - {calculateChapterTime(chapter)}</p>
+                            </div>
+                            <div className={`overflow-hidden transition-all duration-300 ${openSection[index] ? 'max-h-96': 'max-h-0'}`}>
+                              <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
+                                {chapter.chapterContent.map((lecture,i)=>(
+                                  <li key={i} className='flex items-start gap-2 py-1'>
+                                    <img src={assets.play_icon} alt="" 
+                                    className='w-4 h-4 mt-1' />
+                                    <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
+                                      <p>{lecture.lectureTitle}</p>
+                                    <div className='flex gap-2'>{lecture.isPreviewFree && <p onClick={()=>setPlayerData({
+                                      videoId:lecture.lectureUrl.split('/').pop()
+                                    })} className='text-blue-500 cursor-pointer'>Preview</p> }</div>
+                                    <p>{humanizeDuration(lecture.lectureDuration*60*1000,{units:['h','m']})}</p>
+                                    <p></p>
+                                    </div>
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
     </div>
+
+    {/* right column */}
+    <div></div>
+
+  </div>
+</>
+
   )
 }
 
